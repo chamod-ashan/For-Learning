cd393afe259695d7061281b15df8017e706df627
Chamod Ashan <chamoashz0329@gmail.com>
2025-12-20 20:34:55 +0530

    hust few lines of code

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function clickevent(){
     document.write("hello all this is your first java script event");
 }
+/*document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("pid").onclick = function () {
+        alert("clicked the paragraph");
+    };
+});*/
+$(document).ready(function () {
+    $('#pid').click(function () {
+        alert("clicked the paragraph!!!");
+    });
+});
+
