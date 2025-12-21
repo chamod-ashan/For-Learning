731add07d5b3b0e223986afb1102500cbf05dea8
Chamod Ashan <chamoashz0329@gmail.com>
2025-12-21 19:37:17 +0530

    animated

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
         alert("clicked the paragraph");
     };
 });*/
-$(document).ready(function () {
+/*$(document).ready(function () {
     $('#pid').click(function () {
         $(this).hide();
     });
@@ -15,5 +15,30 @@ $(document).ready(function () {
 $(document).ready(function(){
     $("#but").dblclick(function(){
         alert("you double clicked this button!!");
+    })
+})*/
+
+$(document).ready(function(){
+    $(".hide").click(function(){
+        $(".pfast").hide("fast");
+        $(".pslow").hide("slow");
+        $(".ptoggle").hide(2000);
+
+    })
+})
+$(document).ready(function(){
+    $(".show").click(function(){
+        $(".pfast").show("fast");
+        $(".pslow").show("slow");
+        $(".ptoggle").show(2000);
+
+    })
+})
+$(document).ready(function(){
+    $(".toggle").click(function(){
+        $(".pfast").toggle("fast");
+        $(".pslow").toggle("slow");
+        $(".ptoggle").toggle(2000);
+
     })
 })
